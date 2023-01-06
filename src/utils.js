60b329dab1366e4cc241f775ebf49be8f55906a2
lazaronazareno <lazaronazareno@gmail.com>
2023-01-05 22:29:57 -0300

    option categories  utils const

---
+++ src/utils.js
@@ -0,0 +1,30 @@
+export const OptionCategories = [
+  {
+    value: 'general',
+    name: 'General'
+  },
+  {
+    value: 'business',
+    name: 'Negocios'
+  },
+  {
+    value: 'entertainment',
+    name: 'Entretenimiento'
+  },
+  {
+    value: 'health',
+    name: 'Salud'
+  },
+  {
+    value: 'science',
+    name: 'Ciencia'
+  },
+  {
+    value: 'sports',
+    name: 'Deportes'
+  },
+  {
+    value: 'technology',
+    name: 'Tecnologia'
+  }
+]
