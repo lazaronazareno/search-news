da4a99db99e700a65dcda528a3a62d63766f4a47
lazaronazareno <lazaronazareno@gmail.com>
2023-01-06 15:39:48 -0300

    newData format, new 404image, url external link

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,28 +1,32 @@
 import React from 'react'
 import styles from './News.module.css'
+import ImageNotFound from '../assets/image-not-found.png'
 
 const NewsItem = ({ data }) => {
   if (Object.keys(data).length === 0) return null
 
-  const { urlToImage, title, description, author, publishedAt } = data
+  const { urlToImage, url, title, description, author, publishedAt } = data
 
   const correctTitle = title.replace(/^(.*)-.*$/, '$1')
+  const correctDate = publishedAt.replace(/^(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}).*Z$/, '$1 - $2')
 
   const image = (urlToImage)
-    ? <img className={styles.item__img} src={urlToImage} />
-    : null
+    ? <img className={styles.item__img} src={urlToImage} alt={title} />
+    : <img className={styles.item__img} src={ImageNotFound} alt='News wordcloud' />
   return (
     <div className={styles.item_container}>
       {image}
       <div className={styles.item__text_container}>
         <div className={styles.item__author}>
+          <span>{correctDate}</span>
+          {author ? <span> - </span> : null}
           <span>{author}</span>
-          <span> - </span>
-          <span>{publishedAt}</span>
         </div>
         <div className={styles.item__main_text}>
-          <h1>{correctTitle}</h1>
-          <span>{description}</span>
+          <a className={styles.link} href={url} rel='noopener noreferrer' target='_blank'>
+            <h1>{correctTitle}</h1>
+            <span>{description}</span>
+          </a>
         </div>
       </div>
     </div>
